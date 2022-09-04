793868f713af615cdb31f031ae7a5d337b012293
tuselina <110763766+tuselina@users.noreply.github.com>
2022-09-04 16:50:43 +1000

    Add files via upload

--- exercise2/modeSwitch.js
+++ exercise2/modeSwitch.js
@@ -7,12 +7,12 @@ function switchModes(){
     if(isDark === true){
         console.log("im true");
         document.documentElement.style.setProperty("--col-01", "#171219");
-        document.documentElement.style.setProperty("--col-02", "#f2fbeb");
+        document.documentElement.style.setProperty("--col-02", "#e7db95");
         document.getElementById("modeButton").innerHTML = "Dark Mode";
         isDark = false;
     } else {
         console.log("im false");
-        document.documentElement.style.setProperty("--col-01", "#f2fbeb");
+        document.documentElement.style.setProperty("--col-01", "#e7db95");
         document.documentElement.style.setProperty("--col-02", "#171219");
         document.getElementById("modeButton").innerHTML = "Light Mode";
         isDark = true;
